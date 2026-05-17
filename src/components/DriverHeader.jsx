04395001230082cd085586d9932c5494e7481c92
SHAH7766 <shahussainbadshah786@gmail.com>
2026-05-17 19:56:54 +0500

    Pages updated

--- src/components/DriverHeader.jsx
+++ src/components/DriverHeader.jsx
@@ -2,15 +2,11 @@
 
 import React from 'react';
 import Navbar from './Navbar';
-import MobileHeader from './MobileHeader';
 
 export default function DriverHeader() {
   return (
     <header className="w-full">
       <Navbar />
-      <div className="px-4 pt-4 md:hidden">
-        <MobileHeader />
-      </div>
     </header>
   );
 }
